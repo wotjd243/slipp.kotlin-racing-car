21aacb16a0b5df682727c9507511336f3e0366f9
박재성(Jason) <wotjd243@naver.com>
2018-03-07 02:25:30 +0900

    Initial repository

---
+++ src/main/kotlin/net/slipp/racingCar/RacingCarApplication.kt
@@ -0,0 +1,11 @@
+package net.slipp.racingCar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RacingCarApplication
+
+fun main(args: Array<String>) {
+    runApplication<RacingCarApplication>(*args)
+}
---
+++ src/test/kotlin/net/slipp/racingCar/RacingCarApplicationTests.kt
@@ -0,0 +1,16 @@
+package net.slipp.racingCar
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class RacingCarApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
